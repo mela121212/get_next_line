11c771f1427fb39fc52b86452e3239782a63d7a9
mela121212 <144914850+mela121212@users.noreply.github.com>
2023-12-30 21:41:46 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,29 +1,29 @@
-char	*ft_free(char *str)
+char	ft_free(char *str)
 {
-	free(*str);
-	*str = NULL;
+	free(str);
+	str = NULL;
 	return (NULL);
 }
 
 char	fill_stash(int fd, char *stash, char *buffer)
 {
 	char	temp;
-	char	num_char;
+	int	num_char;
 
 	num_char = 1;
 	while (num_char != '\0')
 	{
-		if (num_char = -1)
-			return (0); // ??
-		if (num_char = 0)
-			break;
-		if (!(stash))
-			stash = strdup(""); //SI USO CALLOC HAY QUE USAR EL STRDUP??
 		num_char = read(fd, buffer, BUFFER_SIZE);
+		if (num_char == -1)
+			return (0); // ??
+		if (num_char == 0)
+			break;
+		if (!stash)
+			stash = ft_strdup(""); //SI USO CALLOC HAY QUE USAR EL STRDUP??
 		temp = stash;
-		stash = strjoin(buffer, temp);
+		stash = ft_strjoin(temp, buffer);
 		ft_free(temp);
-		if (strchr(buffer, '\n')) // != 0??
+		if (strchr(buffer, '\n'))
 			break;
 	}
 	return (stash);
@@ -36,31 +36,33 @@ char	extract_line(char *line, char *stash)
 	int	end;
 
 	counter = 0;
-	while (line[counter] != '\n') || (line['\0'] != '\0');
+	while (line[counter] != '\n') && (line[counter] != '\0');
 		counter++;
-	if (line[counter] ==  '\0')
+	if (line[counter] == '\0')
 		return (0);
 	start = counter + 1;
-	end = ft_strlen - 1;
-	stash = substr(line, start, end);
-	if (stash == '\0')
+	end = ft_strlen(line) - 1;
+	stash = ft_substr(line, start, end);
+	if (stash == NULL)
 		ft_free(stash);
 	return (stash);
 }
 
-char	get_next_line(int fd)
+char	*get_next_line(int fd) //por que puntero
 {
 	char	*buf;
 	char	*stash; //static??
 	char	*line;
 
-	buf = (char *)malloc(char) * (BUFFER_SIZE + 1);
-	if (!(buf))
-		return ;
-	line = fill_stash(line, stash, buf);
+	if (fd < 0)
+		return (0);
+	buf = (char *)malloc(sizeof(char)) * (BUFFER_SIZE + 1);
+	if (!buf)
+		return (0);
+	line = fill_stash(fd, stash, buffer);
+	ft_free(buf);
 	stash = extract_line(line, stash);
-	return (stash);
-	
+	return (line);
 }
 
 int	main(void)
