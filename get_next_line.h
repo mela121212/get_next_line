b802fb73ec96b3366821d8feb4ba30a0d3aeb00f
mela121212 <144914850+mela121212@users.noreply.github.com>
2023-12-30 22:36:43 +0100

    get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,1 +1,16 @@
+#ifndef GET_NEXT_LINE_H
+#  define GET_NEXT_LINE_H
 
+#  include <stdlib.h>
+#  include <>
+
+char  *get_next_line(int fd);
+char  fill_stash(int fd, char *buffer, char *stash)
+char  extract_line(line, buffer)
+char  ft_free(char *str)
+char  ft_strjoin(char *s1, char s2);
+int    ft_strlen(
+char  ft_strchr(
+char  ft_substr(
+
+#  endif
